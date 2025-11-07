25e4caa0e58ae1cabbbeb97e88614aac07161bd2
ARIBA-HASAN <aribahasan04@gmail.com>
2025-11-07 13:35:53 +0600

    GenreController and homeWithGenre.fxml and .css

--- bookSharing/first_draft/src/main/java/com/example/first_draft/GenreController.java
+++ bookSharing/first_draft/src/main/java/com/example/first_draft/GenreController.java
@@ -3,10 +3,14 @@ package com.example.first_draft;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -39,7 +43,47 @@ public class GenreController {
                     e.printStackTrace();
                 }
             });
+
             vbox.getChildren().add(genreButton);
+            List<Book> booksInGenre = g.getBooks();
+            HBox displayBox = new HBox();
+
+            VBox book1 = new VBox();
+            Label name1 = new Label(booksInGenre.get(0).getTitle());
+            Label author1 = new Label(booksInGenre.get(0).getAuthor());
+            ImageView cover1 = booksInGenre.get(0).getCover();
+            cover1.setFitWidth(110);
+            cover1.setFitHeight(130);
+            cover1.setPreserveRatio(true);
+            book1.getChildren().addAll(cover1, name1, author1);
+            book1.setAlignment(Pos.TOP_CENTER);
+            displayBox.getChildren().add(book1);
+
+            VBox book2 = new VBox();
+            Label name2 = new Label(booksInGenre.get(1).getTitle());
+            Label author2 = new Label(booksInGenre.get(1).getAuthor());
+            ImageView cover2 = booksInGenre.get(1).getCover();
+            cover2.setFitWidth(110);
+            cover2.setFitHeight(130);
+            cover2.setPreserveRatio(true);
+            book2.getChildren().addAll(cover2, name2, author2);
+            book2.setAlignment(Pos.TOP_CENTER);
+            displayBox.getChildren().add(book2);
+            displayBox.spacingProperty().bind(displayBox.widthProperty().multiply(0.05));
+            displayBox.setAlignment(Pos.CENTER);
+
+//            VBox book3 = new VBox();
+//            Label name3 = new Label(booksInGenre.get(2).getTitle());
+//            Label author3 = new Label(booksInGenre.get(2).getAuthor());
+//            ImageView cover3 = booksInGenre.get(2).getCover();
+//            cover3.setFitWidth(120);
+//            cover3.setFitHeight(160);
+//            cover3.setPreserveRatio(true);
+//            book1.getChildren().addAll(cover3, name3, author3);
+//            displayBox.getChildren().add(book3);
+            vbox.getChildren().add(displayBox);
+            vbox.setAlignment(Pos.TOP_CENTER);
+
         }
     }
 
